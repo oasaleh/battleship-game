dcb276dd3803ad5cd6637cfd5750f0ccd4909975
Omar <omarasaleh@hotmail.com>
2021-07-18 11:04:16 -0500

    Add logic to check if ship out of board coords

--- factories/gameboard.mjs
+++ factories/gameboard.mjs
@@ -1,39 +1,51 @@
 import { Ship } from './ship.mjs';
 
-export function Gameboard() {
-  this.grid = [];
-  this.createGrid = function () {
+function Gameboard() {
+  this.board = [];
+  this.createBoard = function () {
     for (let i = 0; i < 10; i++) {
-      this.grid.push([]);
+      this.board.push([]);
       for (let k = 0; k < 10; k++) {
-        this.grid[i].push('');
+        this.board[i].push({ hasShip: false, isShot: false });
       }
     }
-    return this.gird;
+    return this.board;
   };
-  this.deploy = function (shipObject, coordinatesObject, deploymentCoords) {
-    
-  }
+  this.receiveShot = function (coordX, coordY) {
+    this.board[coordY][coordX].isShot = true;
+  };
+
+  this.checkIfShotHit = function (coordX, coordY) {
+    // return true for hit, false for miss
+    return this.board[coordY][coordX].hasShip;
+  };
+  this.determineShipLocation = function (coordX, coordY, ship, axis) {
+    const shipLocation = [];
+    for (let i = 0; i < ship.size; i += 1) {
+      if (axis === 'horizontal') {
+        // shipLocation[coordY] = coordY;
+        shipLocation.push([coordX + i, coordY]);
+      } else {
+        shipLocation.push([coordX, coordY + i]);
+      }
+    }
+    return shipLocation;
+  };
+  this.outOfBoardBounds = function (shipLocation) {
+    return shipLocation.every((cords) => cords[0] <= 10 && cords[1] <= 10);
+    // for (let i = 0; i < shipLocation.length; i += 1) {
+    //   for (let k = 0; k < shipLocation[i].length; k += 1) {
+    //     if (shipLocation[i][k] > 10) {
+    //       return false;
+    //     }
+    //   }
+    // }
+    // return true;
+  };
 }
-let gameboard = new Gameboard();
-gameboard.createGrid();
-console.log(gameboard.grid);
-
-// this.shipsNeeded = {
-//   carrier: 1,
-//   battleship: 1,
-//   destroyer: 1,
-//   submarine: 1,
-//   boat: 1,
-// };
-
-// this.getShips = function () {
-//   let ships = [];
-//   for (const key in shipsNeeded) {
-//     for (const i = 0; i < shipsNeeded[key]; i++) {
-//       ships.push(new Ship(key));
-//     }
-//   }
-//   return ships;
-// };
-// this.ships = getShips(shipsNeeded);
+const carrier = new Ship('carrier');
+const board = new Gameboard();
+const player1Board = board.createBoard();
+const play1CarrierLocation = board.determineShipLocation(4, 5, carrier, 'horizontal');
+console.log(play1CarrierLocation);
+console.log(board.outOfBoardBounds(play1CarrierLocation));
