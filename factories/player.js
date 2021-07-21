a3d058a6e45d96716b6be61107b48850db1420dd
Omar <omarasaleh@hotmail.com>
2021-07-20 19:27:00 -0500

    Add status active status function to Player

--- factories/player.js
+++ factories/player.js
@@ -3,6 +3,9 @@ import Gameboard from './gameboard';
 function Player(name) {
   this.name = name;
   this.gameboard = new Gameboard();
+  this.active = false;
 }
-
+Player.prototype.toggleActive = function toggleActive() {
+  this.active = !this.active;
+};
 export default Player;
